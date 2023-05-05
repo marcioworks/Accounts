347b7dc315bf48d8b8ed99a83a1e59f1bdfbca10
marcio silva <marcioadsworks@gmail.com>
2023-05-04 21:23:04 -0300

    get properties from config server

--- src/main/java/br/com/marcioss/accounts/controller/AccountsController.java
+++ src/main/java/br/com/marcioss/accounts/controller/AccountsController.java
@@ -1,9 +1,15 @@
 package br.com.marcioss.accounts.controller;
 
+import br.com.marcioss.accounts.config.AccountsServiceConfig;
 import br.com.marcioss.accounts.model.Accounts;
 import br.com.marcioss.accounts.model.Customer;
+import br.com.marcioss.accounts.model.Properties;
 import br.com.marcioss.accounts.repository.AccountsRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +19,19 @@ public class AccountsController {
     @Autowired
     private AccountsRepository accountsRepository;
 
+    @Autowired
+    private AccountsServiceConfig serviceConfig;
+
     @PostMapping("/myAccount")
-    public Accounts getAccountDetails(@RequestBody Customer customer){
+    public Accounts getAccountDetails(@RequestBody Customer customer) {
         return accountsRepository.findByCustomerId(customer.getCustomerId());
+    }
+
+    @GetMapping("/accounts/properties")
+    public String getPropertiesDetails() throws JsonProcessingException {
+        ObjectWriter objectWriter = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(serviceConfig.getMsg(), serviceConfig.getBuildVersion(),
+                serviceConfig.getMailDetails(), serviceConfig.getActiveBranches());
+        return objectWriter.writeValueAsString(properties);
     }
 }
